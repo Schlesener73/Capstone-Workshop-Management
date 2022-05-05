5a16f43a168df3a4088b998a814e2a4445dc5257
James Schlesener <schlesener73@gmail.com>
2022-05-04 21:58:17 -0400

    updated final reports

--- Docs/Reports/RS-10 Only One/src/WM-client/src/app/convert2ampm.pipe.spec.ts
+++
@@ -1,9 +0,0 @@
-import { PhonePipe } from './phone.pipe';
-import { String2ampmPipe } from './convert2ampm.pipe';
-
-describe('String2ampmPipe', () => {
-  it('create an instance', () => {
-    const pipe = new String2ampmPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
